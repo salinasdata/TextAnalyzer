2c5bd779dbb1acbdeda60da0f62e0f96b0d0c746
Adriana Shymoniak <adrianashymoniak@gmail.com>
2023-07-19 18:35:28 +0300

    feat: add error exception

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import openai
 import uvicorn
 from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
+from openai.error import AuthenticationError
 
 from analyzer import Analyzer
 
@@ -51,62 +52,68 @@ async def websocket_endpoint(websocket: WebSocket):
             is_http = input.strip().lower().startswith('http')
             is_url = True if input is not None and is_http else False
             is_openai_key = True if openai_key is not None and '-' in openai_key else False
-            if is_url and is_openai_key:
-                await websocket.send_text(
-                    "Details saved. Please do not reload the page!"
-                )
-                await asyncio.sleep(0.01)
-                await websocket.send_text("Set OpenAI secret key!")
-                await asyncio.sleep(0.01)
-                openai.api_key = openai_key
-                os.environ['OPENAI_API_KEY'] = openai_key
+            try:
+                if is_url and is_openai_key:
+                    await websocket.send_text(
+                        "Details saved. Please do not reload the page!"
+                    )
+                    await asyncio.sleep(0.01)
+                    await websocket.send_text("Set OpenAI secret key!")
+                    await asyncio.sleep(0.01)
+                    openai.api_key = openai_key
+                    os.environ['OPENAI_API_KEY'] = openai_key
 
-                await websocket.send_text("Analyzing...")
-                await asyncio.sleep(0.01)
-                await websocket.send_text("Fetching files from public repository...")
-                await asyncio.sleep(0.01)
-                files, owner, repo_name = await Analyzer.aget_files_from_dir(
-                    input, websocket
-                )
-                await websocket.send_text("Files are fetched!")
-                await asyncio.sleep(0.01)
-                await websocket.send_text("Iterating over fetched files...")
-                await asyncio.sleep(0.01)
-                if files and owner and repo_name:
-                    files = await Analyzer.aread_files(file_paths=files,
-                                                       owner=owner,
-                                                       repo_name=repo_name)
-                    items = files.items()
-                    for _file, _content in items:
-                        code_analyzer = Analyzer(_content, _file)
-                        await code_analyzer.aanalyze_file(websocket)
+                    await websocket.send_text("Analyzing...")
+                    await asyncio.sleep(0.01)
+                    await websocket.send_text("Fetching files from public repository...")
+                    await asyncio.sleep(0.01)
+                    files, owner, repo_name = await Analyzer.aget_files_from_dir(
+                        input, websocket
+                    )
+                    await websocket.send_text("Files are fetched!")
+                    await asyncio.sleep(0.01)
+                    await websocket.send_text("Iterating over fetched files...")
+                    await asyncio.sleep(0.01)
+                    if files and owner and repo_name:
+                        files = await Analyzer.aread_files(file_paths=files,
+                                                           owner=owner,
+                                                           repo_name=repo_name)
+                        items = files.items()
+                        for _file, _content in items:
+                            code_analyzer = Analyzer(_content, _file)
+                            await code_analyzer.aanalyze_file(websocket)
 
-                    await websocket.send_text("All files processed!")
+                        await websocket.send_text("All files processed!")
+                        await asyncio.sleep(0.01)
+                    else:
+                        await websocket.send_text("Something happen with the API")
+                        await asyncio.sleep(0.01)
+                elif not is_url and is_openai_key:
+                    await websocket.send_text(
+                        "Details saved. Please do not reload the page!"
+                    )
+                    await asyncio.sleep(0.01)
+                    await websocket.send_text("Set OpenAI secret key!")
+                    await asyncio.sleep(0.01)
+                    openai.api_key = openai_key
+                    os.environ['OPENAI_API_KEY'] = openai_key
+
+                    await websocket.send_text("Analyzing...")
+                    await asyncio.sleep(0.01)
+                    code_analyzer = Analyzer(input)
+                    await code_analyzer.aanalyze_file(websocket, is_github=False)
                     await asyncio.sleep(0.01)
                 else:
-                    await websocket.send_text("Something happen with the API")
+                    await websocket.send_text("Please check provided details")
                     await asyncio.sleep(0.01)
-            elif not is_url and is_openai_key:
-                await websocket.send_text(
-                    "Details saved. Please do not reload the page!"
-                )
-                await asyncio.sleep(0.01)
-                await websocket.send_text("Set OpenAI secret key!")
-                await asyncio.sleep(0.01)
-                openai.api_key = openai_key
-                os.environ['OPENAI_API_KEY'] = openai_key
 
-                await websocket.send_text("Analyzing...")
+                await websocket.send_text('Analyzing completed.')
                 await asyncio.sleep(0.01)
-                code_analyzer = Analyzer(input)
-                await code_analyzer.aanalyze_file(websocket, is_github=False)
+            except AuthenticationError as e:
+                await websocket.send_text(f"<pre style='white-space: "
+                                          f"pre-wrap; color:red;'"
+                                          f">{e}</pre>")
                 await asyncio.sleep(0.01)
-            else:
-                await websocket.send_text("Please check provided details")
-                await asyncio.sleep(0.01)
-
-            await websocket.send_text('Analyzing completed.')
-            await asyncio.sleep(0.01)
 
     except WebSocketDisconnect as e:
         print('error:', e)
